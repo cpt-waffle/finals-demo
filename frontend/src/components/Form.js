fdac573b2f84f8bca7face0d7ce9184870c7c431
vasiliy-klimkin <vasily.klimkin@gmail.com>
2022-06-09 12:02:44 -0400

    add form

---
+++ frontend/src/components/Form.js
@@ -0,0 +1,11 @@
+
+const Form = props => {
+  return (<form>
+   <p>First Name<input type='text' name="name"/></p>
+   <p>Last Name<input type='text' name="name"/></p>
+
+  </form>);
+}
+
+
+export default Form;
