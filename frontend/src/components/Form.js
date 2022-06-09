94450f29437fa84a3acde580f0a17bb04ba4882b
vasiliy-klimkin <vasily.klimkin@gmail.com>
2022-06-09 12:16:05 -0400

    spacing

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -5,6 +5,7 @@ const Form = props => {
     <p>First Name<input type='text' name="name"/></p>
     <p>Last Name<input type='text' name="name"/></p>
     <p>Credit Card<input type='text' name="card"/></p>
+    
     <button> Submit</button>
   </form>);
 }
