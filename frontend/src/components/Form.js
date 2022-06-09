6a0eb5ae5669f57fa82b1a26744b5f705f0290fd
vasiliy-klimkin <vasily.klimkin@gmail.com>
2022-06-09 12:15:51 -0400

    add card input

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -4,6 +4,7 @@ const Form = props => {
   return (<form className="form">
     <p>First Name<input type='text' name="name"/></p>
     <p>Last Name<input type='text' name="name"/></p>
+    <p>Credit Card<input type='text' name="card"/></p>
     <button> Submit</button>
   </form>);
 }
